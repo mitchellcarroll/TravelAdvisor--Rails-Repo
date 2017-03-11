670f8c3bc01f929f3b9b7bef7517d1099c8d1320
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.local>
2017-03-11 15:59:26 -0500

    Created Migrations

---
+++ traveladvisor/db/migrate/20170311203455_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[5.0]
+  def change
+    create_table :comments do |t|
+      t.string :author, null: false, default: "Anonymous"
+      t.string :content, null: false
+      t.timestamps
+      t.references :reviews, foreign_key: true
+    end
+  end
+end
--- traveladvisor/db/seeds.rb
+++ traveladvisor/db/seeds.rb
@@ -5,3 +5,61 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Review.destroy_all
+Comment.destroy_all
+
+reviews = Review.create = [(
+  {
+    author: "Borat",
+    title: "Greatest Country In The World",
+    content: "Kazakhstan is the greatest country in the world!  All other countries are run by little girls!",
+    photoUrl: ""
+  },
+  {
+    author: "Jesse Pinkman",
+    title: "Kazakhstan Beeyatch!",
+    content: "Kazakhstan has the bluest skies!",
+    photoUrl: ""
+  },
+  {
+    author: "Orange Man",
+    title: "Make Kazakhstan Great Again",
+    content: "Kazakhstan did not live up to expectations, but I will make it great again!",
+    photoUrl: ""
+  },
+  {
+    author: "Peter Griffin",
+    title: "Not Family-Friendly!",
+    content: "They stared at my wife a lot, and this Meg girl kept bothering us.",
+    photoUrl: ""
+  },
+  {
+    author: "Chandler Bing",
+    title: "I was going to Yemen!",
+    content: "I was supposed to go to Yemen and ended up here!  I miss Monica...",
+    photoUrl: ""
+  }
+)]
+
+comments = Comment.create = [(
+  {
+    author: "Kazakhstan President Nazarbayev",
+    content: "Borat is humiliating my country."
+  },
+  {
+    author: "Heisenberg",
+    content: "I am the one who knocks in Kazakhstan!"
+  },
+  {
+    author: "Barack Obama",
+    content: "Kazakhstan just needs some ObamaCare."
+  },
+  {
+    author: "Borat",
+    content: "Kazakhstan is very safe country!"
+  },
+  {
+    author: "Janice",
+    content: "I knew you were just running away!"
+  }
+)]
