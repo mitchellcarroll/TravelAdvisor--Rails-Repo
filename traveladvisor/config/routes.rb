b46f663bc7b40c97c06cd627304b456b517d83ad
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-03-12 16:52:45 -0400

    Updated Comments and Reviews Controllers

--- traveladvisor/config/routes.rb
+++ traveladvisor/config/routes.rb
@@ -4,14 +4,9 @@ Rails.application.routes.draw do
 
     end
   end
-<<<<<<< HEAD
-  resources :reviews do
-    resources :comments
-  end
-=======
+
   root to: 'reviews#index'
   get '/test_ajax', to: 'reviews#test_ajax'
->>>>>>> 19a59d435bc811b0e2534453b9437ac13a6ecb50
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    resources :reviews do
