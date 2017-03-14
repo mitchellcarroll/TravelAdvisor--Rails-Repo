b26ba460a5e28be3474060b4fc632f6d369ee106
shapiro_benjamin@bah.com <benjaminshapiro@C1MS50ZQH3QK.fios-router.home>
2017-03-14 17:23:04 -0400

    Navigation Works

--- traveladvisor/app/controllers/attractions_controller.rb
+++ traveladvisor/app/controllers/attractions_controller.rb
@@ -1,13 +1,13 @@
 class AttractionsController < ApplicationController
 
   def index
-    
+
     @attractions = Attraction.all
     render json: @attractions
   end
 
   def show
-    @attraction = Attraction.find(params[:location_id])
+    @attraction = Attraction.find(params[:id])
        render  json: @attraction
   end
 
